4c4e92f12171a1e50e70f4b3526b565249619707
Богдан <ress@ress.home>
2017-02-13 16:29:08 +0700

    Input/Output, main program

--- main.c
+++ main.c
@@ -1,7 +1,38 @@
 #include <iostream>
 
+int getvklad()
+{
+  int x;
+  cout << "Enter Vklad: ";
+  cin >> x;
+  return x;
+}
+
+int getsrok()
+{
+  int x;
+  cout << endl << "Enter Srok: ";
+  cin >> x;
+  return x;
+}
+
 int main()
 {
-	system("PAUSE");
-	return 0;
+  int vklad = getvlkad();
+  int srok = getsrok();
+  float dohod = 0;
+
+  if (vklad > 100) a = 1.03; else a = 1.02;
+  if (vklad > 100) a = 1.08; else a = 1.06;
+  if (vklad > 100) a = 1.15; else a = 1.12;
+  
+  if (srok<=30) dohod = vklad*0.9;
+  if (srok>30 && srok<121) dohod = vklad * a;
+  if (srok>120 && srok<241) dohod = vklad * b;
+  if (srok>240 && srok<366) dohod = vklad * c;
+
+  cout << endl << "Dohod: " << (int)dohod;
+  
+  system("PAUSE");
+  return 0;
 }
